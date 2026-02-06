c7e2c5d49bede68c0aeead8cab2ccf9489eef823
9rajputshivam <9rajputshivam@gmail.com>
2026-02-06 12:29:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,11 +16,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 /* ---------------- Timezone Logic ---------------- */
 const countryTimezones = {
-  india: 'Asia/Kolkata',
+  austria: 'Europe/Vienna',
+  belgium: 'Europe/Brussels',
+  bulgaria: 'Europe/Sofia',
+  croatia: 'Europe/Zagreb',
+  cyprus: 'Asia/Nicosia',
+  czechia: 'Europe/Prague',
+  denmark: 'Europe/Copenhagen',
+  estonia: 'Europe/Tallinn',
+  finland: 'Europe/Helsinki',
+  france: 'Europe/Paris',
   germany: 'Europe/Berlin',
-  usa: 'America/New_York',
-  uk: 'Europe/London',
-  slovakia: 'Europe/Bratislava'
+  greece: 'Europe/Athens',
+  hungary: 'Europe/Budapest',
+  ireland: 'Europe/Dublin',
+  italy: 'Europe/Rome',
+  latvia: 'Europe/Riga',
+  lithuania: 'Europe/Vilnius',
+  luxembourg: 'Europe/Luxembourg',
+  malta: 'Europe/Malta',
+  netherlands: 'Europe/Amsterdam',
+  poland: 'Europe/Warsaw',
+  portugal: 'Europe/Lisbon',
+  romania: 'Europe/Bucharest',
+  slovakia: 'Europe/Bratislava',
+  slovenia: 'Europe/Ljubljana',
+  spain: 'Europe/Madrid',
+  sweden: 'Europe/Stockholm'
+
 };
 
 function evaluateDaytimeWindow(country) {
@@ -81,3 +104,4 @@ app.post('/activity/stop', (req, res) => res.status(200).json(req.body));
 app.listen(PORT, () => {
   console.log(`🚀 Daytime Window Check running on port ${PORT}`);
 });
+
