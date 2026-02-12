3785c31ce9a582fc687775e396f2137c4f0c5970
9rajputshivam <9rajputshivam@gmail.com>
2026-02-12 15:21:51 +0530

    Update customActivity.js

--- public/customActivity.js
+++ public/customActivity.js
@@ -15,7 +15,7 @@ window.onload = function () {
     // REQUIRED inArguments (must exist)
     payload.arguments.execute.inArguments = [
       {
-        country: "{{Interaction.Country}}"|| "Austria"
+        country: "{{Event.DEAudience-33bd01d3-3b0f-6d57-b79f-2de2718a2b42.Country}}"
       }
     ];
 
@@ -29,5 +29,6 @@ window.onload = function () {
     connection.trigger("updateActivity", payload);
   });
 };
+
 
 
